f128ed3e00339346736061065c143d3e44211147
Alisa0908 <alisa0908piano@gmail.com>
2021-09-16 09:58:32 +0900

    挨拶アプリできました

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('comments/{greeting}', [App\Http\Controllers\GreetingController::class, 'greeting']);
